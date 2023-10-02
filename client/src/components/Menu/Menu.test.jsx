1ea37ac941dd9a54e8b2fec219821508b5134636
Thiago <teyextreme@outlook.com>
2023-10-02 13:36:49 -0300

    Menu component

--- client/src/components/Menu/Menu.test.jsx
+++ client/src/components/Menu/Menu.test.jsx
@@ -2,8 +2,30 @@ import { screen } from '@testing-library/react';
 import { Menu } from '.';
 import { renderTheme } from '../../styles/render-theme';
 
+import linksMock from '../NavLinks/mock';
+import { theme } from '../../styles/theme';
+const logoData = {
+  text: 'Logo',
+  link: '#target',
+};
+
 describe('<Menu />', () => {
-  it('should render', () => {
-    renderTheme(<Menu />);
+  it('should render Menu component', () => {
+    renderTheme(<Menu links={linksMock} logoData={logoData} />);
+    expect(
+      screen.getByRole('navigation', { name: 'Main menu' }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render menu mobile and buttom for open and close menu', () => {
+    renderTheme(<Menu links={linksMock} logoData={logoData} />);
+
+    const button = screen.getByLabelText('Open/Close menu');
+    const menuContainer = button.nextSibling;
+
+    expect(button).toHaveStyleRule('display', 'none');
+    // expect(button).toHaveStyleRule('display', 'flex', {
+    //   media: theme.media.lteMedium,
+    // });
   });
 });
--- client/src/components/Menu/index.jsx
+++ client/src/components/Menu/index.jsx
@@ -1,20 +1,38 @@
 import P from 'prop-types';
 import * as Styled from './styles';
+import { useState } from 'react';
 
 import { SectionContainer } from '../SectionContainer';
 import { LogoLink } from '../LogoLink';
 import { NavLinks } from '../NavLinks';
+import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
 export const Menu = ({ links = [], logoData }) => {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <Styled.Container>
-      <SectionContainer>
-        <Styled.MenuContainer>
-          <LogoLink {...logoData} />
-          <NavLinks links={links} />
-        </Styled.MenuContainer>
-      </SectionContainer>
-    </Styled.Container>
+    <>
+      <Styled.Button
+        aria-label="Open/Close menu"
+        visible={visible}
+        onClick={() => setVisible(true)}
+      >
+        {visible ? (
+          <CloseIcon aria-label="Open menu" />
+        ) : (
+          <MenuIcon aria-label="Close menu" />
+        )}
+      </Styled.Button>
+      <Styled.Container onClick={() => setVisible(false)} visible={visible}>
+        <SectionContainer>
+          <Styled.MenuContainer>
+            <LogoLink {...logoData} />
+            <NavLinks links={links} />
+          </Styled.MenuContainer>
+        </SectionContainer>
+      </Styled.Container>
+    </>
   );
 };
 
